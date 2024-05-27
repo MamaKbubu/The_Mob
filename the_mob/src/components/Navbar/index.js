43dd0a4afaa8234219adf571161c9a886a5deb05
MamaKbubu <maboelatk@gmail.com>
2024-05-27 06:53:45 +0200

    handling pages

--- the_mob/src/components/Navbar/index.js
+++ the_mob/src/components/Navbar/index.js
@@ -23,13 +23,13 @@ const Navbar = ({ toggle }) => {
           </MobileIcon>
           <NavMenu>
             <NavItem>
-              <NavLinks to="about">About</NavLinks>
+              <NavLinks to="/about">About</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="discover">Discover</NavLinks>
+              <NavLinks to="/discover">Discover</NavLinks>
             </NavItem>
             <NavItem>
-              <NavLinks to="services">Services</NavLinks>
+              <NavLinks to="/services">Services</NavLinks>
             </NavItem>
             <NavItem>
               <NavLinks to="signup">Sign Up</NavLinks>
