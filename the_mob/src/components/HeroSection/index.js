a2879b836cac1ae0d6dfe631222dd3a5fb5a9db3
MamaKbubu <maboelatk@gmail.com>
2024-05-27 19:56:19 +0200

    new things

--- the_mob/src/components/HeroSection/index.js
+++ the_mob/src/components/HeroSection/index.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import Video from "../../videos/video.mp4";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+import { Button } from "react-scroll";
 
 const Hero = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
   return (
     <HeroContainer>
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBg>
+      <HeroContent>
+        <HeroH1>THE MOB</HeroH1>
+        <HeroP>
+          Sign up for a new account today and we will do the marketing for
+          you!!!
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   );
 };
