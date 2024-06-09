a5fd372118f59e60b7bb359f48c52a797b184b10
MamaKbubu <maboelatk@gmail.com>
2024-06-09 23:06:11 +0200

    this sign up n sign in is done

--- the_mob/src/components/SignIn/index.js
+++ the_mob/src/components/SignIn/index.js
@@ -20,41 +20,59 @@ import {
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isSigningUp, setIsSigningUp] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await signInWithEmailAndPassword(auth, email, password);
-      console.log("you signed in");
+      if (isSigningUp) {
+        await createUserWithEmailAndPassword(auth, email, password);
+        console.log("Account created");
+      } else {
+        await signInWithEmailAndPassword(auth, email, password);
+        console.log("You are in");
+      }
     } catch (error) {
-      console.error("oh no you didnt", error);
+      console.error("Error:", error.message);
     }
   };
+
   return (
     <>
       <Container>
         <FormWrap>
           <Icon to="/">Canine Client</Icon>
           <FormContent>
-            <Form action="#" onSubmit={handleSubmit}>
-              <FormH1>Sign in to your account</FormH1>
-              <FormLabel htmlFor="for">Email</FormLabel>
+            <Form onSubmit={handleSubmit}>
+              <FormH1>
+                {isSigningUp
+                  ? "Create a new account"
+                  : "Sign in to your account"}
+              </FormH1>
+              <FormLabel htmlFor="email">Email</FormLabel>
               <FormInput
                 type="email"
+                id="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
               />
-              <FormLabel htmlFor="for">Password</FormLabel>
+              <FormLabel htmlFor="password">Password</FormLabel>
               <FormInput
                 type="password"
+                id="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
               />
-              <FormButton type="button">Sign UP</FormButton>
-
-              <FormButton type="submit">Sign In</FormButton>
+              <FormButton type="submit">
+                {isSigningUp ? "Sign Up" : "Sign In"}
+              </FormButton>
+              <Text onClick={() => setIsSigningUp(!isSigningUp)}>
+                {isSigningUp
+                  ? "Already have an account? Sign In"
+                  : "Don't have an account? Sign Up"}
+              </Text>
               <Text>Forgot password</Text>
             </Form>
           </FormContent>
